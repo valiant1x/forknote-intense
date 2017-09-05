f426280ce97549dcee58529faaccd807b7d49dfe
valiant1x <valiant1x@users.noreply.github.com>
2017-09-05 12:48:23 -0400

    Initial commit

---
+++ external/rocksdb/util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h" 
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:Unknown"; 
+const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Tue 09/05/17 12:02 PM"; 
+const char* rocksdb_build_compile_date = __DATE__; 
